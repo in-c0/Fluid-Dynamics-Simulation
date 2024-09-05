1ae31fe33e17388d8f00c031554c647e08c0d3db
in-c0 <wldud5192@gmail.com>
2024-09-05 15:14:03 +1000

    Compute Shader particles generation initial setup

---
+++ shaders/compile.bat
@@ -0,0 +1,4 @@
+C:/VulkanSDK/1.3.290.0/Bin/glslc.exe quad.vert -o vert.spv
+C:/VulkanSDK/1.3.290.0/Bin/glslc.exe quad.frag -o frag.spv
+C:/VulkanSDK/1.3.290.0/Bin/glslc.exe shader_compute.comp -o shader_compute.spv
+pause
